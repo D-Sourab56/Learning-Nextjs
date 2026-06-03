f071d4212eec016a9a008124ad69c47bff5bdf45
Pokhrel Indra <w25017@osfl.ac.jp>
2026-06-03 14:40:01 +0900

    updated

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -1,9 +1,15 @@
 import Hero from '@/app/_components/Hero';
 import Sheet from '@/app/_components/Sheet';
 
+export const metadata = {
+  title: 'ニュース',
+};
+
 type Props = {
   children: React.ReactNode;
 };
+
+export const revalidate = 60;
 
 export default function NewsLayout({ children }: Props) {
   return (
@@ -12,4 +18,4 @@ export default function NewsLayout({ children }: Props) {
       <Sheet>{children}</Sheet>
     </>
   );
-}
+} 
